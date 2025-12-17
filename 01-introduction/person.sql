9b987dd12419eab725d656d67c481ac78d15532b
unknown <sanjayjshenoy20896@gmail.com>
2025-12-17 13:27:59 +0530

    select query with constaints

--- 01-introduction/person.sql
+++ 01-introduction/person.sql
@@ -1,8 +1,9 @@
 create table person (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
 	email VARCHAR(150),
-	gender VARCHAR(50) NOT NULL,
+	gender VARCHAR(7) NOT NULL,
 	date_of_birth DATE NOT NULL,
 	country_of_birth VARCHAR(50)
 );
